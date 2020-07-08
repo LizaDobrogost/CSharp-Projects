231155cabca32d5bf1768a8e1dce7d4f552f8eae
LizaDobrogost <52718469+LizaDobrogost@users.noreply.github.com>
2020-07-08 23:00:58 +0300

    Add files via upload

---
+++ Insertion sort/Insertion sort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Insertion_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[10] {5, 7, 2, 9, 7, 5, 11, 19, 3, 7};
+            int i, j,key;
+            for (i = 1; i < 10; i++)
+            {
+                key = numbers[i];
+                j = i - 1;
+                while (j>=0 && numbers[j]>key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j = j - 1;
+                }
+
+                numbers[j + 1] = key;
+            }
+
+            foreach (var h in numbers)
+            {
+                Console.WriteLine(h);
+            }
+        }  
+    }
+}
