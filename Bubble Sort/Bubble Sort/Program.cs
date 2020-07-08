2a11f0da382cc98b0fefc3f08b99c5e20f436e5f
LizaDobrogost <52718469+LizaDobrogost@users.noreply.github.com>
2020-07-08 22:57:40 +0300

    Add files via upload

---
+++ Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[10] {3, 5, 0, 1, 4, 9, 4, 8, 3, 7};
+            int i, j,buf;
+            for (i = 0; i < 10 - 1; i++)
+            {
+                for (j = 0; j < 10 - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        buf = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = buf;
+                    }
+                }
+            }
+
+            for (int k = 0; k < 9; k++)
+            {
+                 Console.WriteLine($"Sorted:{k}",numbers[k]);
+            }
+
+
+        }
+    }
+}
