4f4a1379a2ceca3ece740e815515b735c3a3dc99
LizaDobrogost <52718469+LizaDobrogost@users.noreply.github.com>
2020-07-08 22:55:41 +0300

    Add files via upload

---
+++ Timer/Timer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = new Stopwatch();
+            TimeSpan duration;
+            DateTime start = default, stop = default;
+            bool menu = true;
+            var key = 0;
+            while (menu)
+            {
+                Console.WriteLine("1-Start timer 2-Stop timer 3-Duration 4-Exit");
+                key = Int32.Parse(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        if (!time.Active)
+                        {
+                            start = time.Start();
+                            Console.WriteLine("Time:" + start.Hour + ":" + start.Minute + ":" + start.Second);
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("Timer is already running!");
+                        }
+                        break;
+                    case 2:
+                        if (time.Active)
+                        {
+                            stop = time.Stop();
+                            Console.WriteLine("Time:" + stop.Hour + ":" + stop.Minute + ":" + stop.Second);
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("Timer is already stopped!");
+                        }
+
+                        break;
+                    case 3:
+                        duration = stop - start;
+                        Console.WriteLine("Duration is:" + duration.Hours + ":" + duration.Minutes + ":" + duration.Seconds);
+                        break;
+                    case 4:
+                        menu = false;
+                        break;
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Timer/Timer/StopWatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Stopwatch
+    {
+        private DateTime start;
+        private DateTime stop { get; set; }
+        private bool isActive;
+
+        public DateTime str
+        {
+            get => start;
+            set
+            {
+                start = value;
+            }
+        }
+
+        public bool Active
+        {
+            get => isActive;
+            set
+            {
+                isActive = value;
+            }
+        }
+        public DateTime Start()
+        {
+            start = new DateTime();
+            start = DateTime.Now;
+            isActive = true;
+            return start;
+        }
+        public DateTime Stop()
+        {
+            stop = new DateTime();
+            stop = DateTime.Now;
+            isActive = false;
+            return stop;
+        }
+
+    }
+}
