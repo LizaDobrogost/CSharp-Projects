b3c868ccc58b118ba40d8aa4e1a91e3944c94812
LizaDobrogost <52718469+LizaDobrogost@users.noreply.github.com>
2020-07-08 22:58:54 +0300

    Add files via upload

---
+++ Selection Sort/Selection Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[10] {3, 8, 1, 3, 3, 4, 9, 6, 9, 1};
+            int min_ind=0,i,j,buf;
+            for (i = 0; i < 9; i++)
+            {
+              min_ind = i;
+              for (j = i+1; j < 10; j++)
+              {
+                  if (numbers[j] < numbers[min_ind])
+                  {
+                      min_ind = j;
+                  }
+                  
+              }
+
+              buf = numbers[min_ind];
+              numbers[min_ind] = numbers[i];
+              numbers[i] = buf;
+            }
+
+            foreach (var il in numbers)
+            {
+                Console.WriteLine(il);
+            }
+
+        }
+    }
+}
