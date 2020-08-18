17d0e80675c2ac55b755d5c13f7aa44b0c16a5cf
LizaDobrogost <52718469+LizaDobrogost@users.noreply.github.com>
2020-08-18 18:33:11 +0300

    Add files via upload

---
+++ PhoneDataBase/PhoneDataBase/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace PhoneDataBase
+{
+    class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Phone> Phones { get; set; }
+
+    }
+}
